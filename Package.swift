a2edb891b7ba16c72049da7ac26cf3d226b3fbc7
Alex Sludnikov <alexsludnikov1@gmail.com>
2024-03-18 16:21:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,10 +20,16 @@ let package = Package(
         .package(url: "https://github.com/pinterest/PINRemoteImage", from: "3.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BFRImageViewer",
-            dependencies: ["PINRemoteImage"]),
+            dependencies: ["PINRemoteImage"],
+            resources: [
+                .process("Resources")
+            ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("."),
+            ]
+        )
     ]
 )
