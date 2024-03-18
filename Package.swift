8ba6f527beefa94f5aced977eb08e88f576fddc7
Alex Sludnikov <alexsludnikov1@gmail.com>
2024-03-18 16:31:35 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/pinterest/PINRemoteImage", from: "3.0.0"),
+        .package(url: "https://github.com/pinterest/PINRemoteImage.git", .branch("master")),
     ],
     targets: [
         .target(
