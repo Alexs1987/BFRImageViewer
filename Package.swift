524c385742c0c4d09711619f0f90c600bfdf5a0c
Alex Sludnikov <alexsludnikov1@gmail.com>
2024-03-22 18:48:57 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,28 +1,27 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "BFRImageViewer",
-    defaultLocalization: "en",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BFRImageViewer",
-            targets: ["BFRImageViewer"]),
+            type: .static,
+            targets: ["BFRImageViewer"]
+        )
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/pinterest/PINRemoteImage.git", .branch("master")),
     ],
     targets: [
         .target(
             name: "BFRImageViewer",
             dependencies: ["PINRemoteImage"],
+            path: "BFRImageViewController",
             resources: [
                 .process("Resources")
             ],
